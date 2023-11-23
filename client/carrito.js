6034adf73183aea167314219444c7fbe88fe5680
Angel Molina Peres <angelmolinaperes@gmail.com>
2023-11-23 10:42:56 -0500

    eliminando comentarios
    Autor: Rivadeneyra

--- client/carrito.js
+++ client/carrito.js
@@ -1,20 +1,16 @@
-// script.js
 
-// Obtener el carrito desde localStorage o inicializarlo si no existe
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-// Llamar a la función para inicializar el carrito
 actualizarCarrito();
 
 function actualizarCarrito() {
     let productosCarrito = document.querySelector('.productos-carrito');
-    productosCarrito.innerHTML = '';  // Limpiar el contenido del carrito
+    productosCarrito.innerHTML = ''; 
 
     carrito.forEach((producto, index) => {
         let fila = document.createElement('div');
         fila.classList.add('fila');
 
-        // Agregar HTML para cada columna
         fila.innerHTML = `
             <div class="col descripcion">
                 <img src="img/shoes${index + 1}.png" alt="">
@@ -39,11 +35,9 @@ function actualizarCarrito() {
             </div>
         `;
 
-        // Agregar fila al carrito
         productosCarrito.appendChild(fila);
     });
 
-    // Actualizar el total de la compra
     actualizarTotalCompra();
 }
 
@@ -58,14 +52,11 @@ function actualizarPrecio(index) {
     let cantidad = carrito[index].cantidad;
     let precioTotal = precioUnitario * cantidad;
 
-    // Actualizar el precio en la interfaz
     let precioElemento = document.getElementById(`precio-${index}`);
     precioElemento.textContent = `S/${precioTotal.toFixed(2)}`;
 
-    // Actualizar el total de la compra
     actualizarTotalCompra();
 
-    // Actualizar el carrito en localStorage
     guardarCarrito();
 }
 
@@ -85,24 +76,18 @@ function eliminarProducto(index) {
     carrito.splice(index, 1);
     actualizarCarrito();
 
-    // Actualizar el carrito en localStorage
     guardarCarrito();
 }
 
 function realizarPago() {
-    // Obtener el total de la compra
     let totalCompra = calcularTotalCompra();
 
-    // Lógica de procesamiento de pago (reemplaza esto con tu lógica real)
     if (totalCompra > 0) {
         alert(`Pago exitoso. Total: S/${totalCompra.toFixed(2)}`);
-        // Aquí podrías realizar acciones adicionales, como limpiar el carrito, etc.
         
-        // Limpiar el carrito
         carrito = [];
         actualizarCarrito();
 
-        // Actualizar el carrito en localStorage
         guardarCarrito();
     } else {
         alert("No hay productos en el carrito para procesar el pago.");
